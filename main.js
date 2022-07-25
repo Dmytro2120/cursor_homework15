3b82fe916451083acf815a4371ac073fc84787ea
Dmytro <godovdmytrro@gmail.com>
2022-07-25 14:05:52 +0200

    homework15

---
+++ main.js
@@ -0,0 +1,42 @@
+
+function* createIdGenerator() {
+    let i = 1;
+    while (i){
+        yield i++;
+    }    
+};
+
+const idGenerator = createIdGenerator();
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+
+
+function* newFontGenerator () {
+    let font = 14;   
+    let move = yield font;
+
+    while (font) {        
+        if (move === "up") {
+            font += 2;            
+        } else if (move ==="down") {
+            font -= 2;
+        } else {
+            font;
+        };
+        
+        move = yield font;
+    }
+}
+
+const fontGenerator = newFontGenerator();
+
+console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next().value);
